bf6d52ce67d71a36fe45c43fec890a4f315f5281
mspragi <msprksh45@gmail.com>
2021-07-16 11:42:47 +0530

    first commit

--- api.js
+++ api.js
@@ -8,6 +8,11 @@ form.addEventListener("submit", (e) =>{
     Searchmovie(SearchKey);
 });
 
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    Searchmovie();
+})
+
 async function Searchmovie(SearchKey){
     let BASE_URL = `http://www.omdbapi.com/?s=${SearchKey}&apikey=${key}&` ;
     try{
@@ -26,7 +31,7 @@ async function Searchmovie(SearchKey){
             btn btn-primary" href="#">Details</a>
             </div>
             </div>`;
-            document.getElementById("template").innerHTML = output;
+        document.getElementById("template").innerHTML = output;
         }
     }
     catch(error){
@@ -34,51 +39,4 @@ async function Searchmovie(SearchKey){
     }
 }
 
-// function movieSelected(id){
-//     sessionStorage.setItem('movieId',id);
-//     window.location = 'movie.html';
-//     return false;
-// }
-
-// function getMovie(){
-//     let movieId = sessionStorage.getItem('movieId');
-//     axios.get(`http://www.omdbapi.com/?s=${movieId}&apikey=${key}&`)
-//     .then((response)=>{
-//         console.log(response);
-//         let movie = response.data;
-//         let output=`
-//         <div class="row>
-//         <div class="col-md-4>
-//         <img src="${movie.Poster}" class="thumbnail>
-//         </div>
-//         <div class="col-md-8>
-//         <h2>${movie.Title}</h2>
-//        <ul class="list-group">
-//        <li class="list-group-item"><strong>Genre:</strong> ${movie.Genre}</li>
-//        <li class="list-group-item"><strong>Released:</strong> ${movie.Released}</li>
-//        <li class="list-group-item"><strong>Rated:</strong> ${movie.Rated}</li>
-//        <li class="list-group-item"><strong>IMDB Rating:</strong> ${movie.imdbRating}</li>
-//        <li class="list-group-item"><strong>Director:</strong> ${movie.Director}</li>
-//        <li class="list-group-item"><strong>Writer:</strong> ${movie.Writer}</li>
-//        <li class="list-group-item"><strong>Actors:</strong> ${movie.Actors}</li>
-//      </ul>
-//    </div>
-//  </div>
-//  <div class="row">
-//    <div class="well">
-//      <h3>Plot</h3>
-//      ${movie.Plot}
-//      <hr>
-//      <a href="http://imdb.com/title/${movie.imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-//      <a href="index.html" class="btn btn-default">Go Back To Search</a>
-//    </div>
-//  </div>
-
-// `;
-//   document.getElementById("template").innerHTML = output;       
-//     })
-//     .catch((err)=>{
-//         console.log(err);
-//     })
-// }
 
